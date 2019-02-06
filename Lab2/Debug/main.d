b3704091df95b84d71ca3bc71fd0760a84493db9
Edgar Guzman <edgarguzman@Edgars-MacBook-Pro.local>
2019-02-05 21:16:15 -0600

    Lab 2 Intial Setup
    Lab 2 initial set-up

---
+++ Lab2/Debug/main.d
@@ -0,0 +1,47 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h
+main.obj: /Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h
+main.obj: /Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: /Applications/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+main.obj: /Applications/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+main.obj: /Applications/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h
+main.obj: /Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+main.obj: /Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+main.obj: /Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h
+main.obj: /Users/edgarguzman/Documents/ECE266\ Labs/ECE266LabGroup7/launchpad.h
+main.obj: ../seg7.h
+
+../main.c: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h: 
+/Applications/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h: 
+/Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+/Applications/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+/Applications/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+/Applications/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h: 
+/Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+/Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+/Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h: 
+/Users/edgarguzman/Documents/ECE266\ Labs/ECE266LabGroup7/launchpad.h: 
+../seg7.h: 
---
+++ Lab2/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llibc.a -l"/Applications/ti/TivaWare_C_Series-2.1.4.178/driverlib/ccs/Debug/driverlib.lib" -l"/Users/edgarguzman/Documents/ECE266\ Labs/ECE266LabGroup7/Debug/Util2.lib"
+
